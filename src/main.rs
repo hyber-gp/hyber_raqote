73ea127027949a80cbb2f4ace6160b40045af4a7
DiogoSemedo <diogo.semedo.uc@gmail.com>
2020-11-27 23:57:59 +0000

    release functional hyber event system on minifb, issue#1

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct Rendererxpto{
     
 }
 
-enum Message{
+enum MessageXPTO{
     Abc,
     Dfg
 }
@@ -24,26 +24,31 @@ enum EventClient{
     right_click
 }
 
-impl hyber::Renderer for Rendererxpto{
-    type Message = Message;
-    fn map_events<EventClient>(eventClient: EventClient) -> hyber::Event { 
+impl hyber::Renderer<Window,EventClient> for Rendererxpto{
+    type Message = MessageXPTO;
+    fn map_events(eventClient: EventClient) -> hyber::Event { 
         match eventClient {
             left_click => hyber::Event::Mouse(hyber::Mouse::ButtonPressed(hyber::MouseButton::Left)),
             right_click => hyber::Event::Mouse(hyber::Mouse::ButtonPressed(hyber::MouseButton::Right))
         }
     }
-    fn detect_sys_events(queue: &hyber::Queue<hyber::Event>) {
-        queue.enqueue(hyber::Event::Mouse(hyber::Mouse::ButtonPressed(hyber::MouseButton::Left)));
-        /*if window.get_mouse_down(minifb::MouseButton::Left) {
-            let event = Rendererxpto::map_events(EventoCliente::left_click);
-            queue.enqueue(event);
-        }*/
+    fn detect_sys_events(queue: &mut hyber::Queue<hyber::Event>,window: &mut Window) {
+        if(window.is_open() && !window.is_key_down(Key::Escape)){
+            if window.get_mouse_down(minifb::MouseButton::Left) {
+                let event = Rendererxpto::map_events(EventClient::left_click);
+                queue.enqueue(event);
+            }
+        }
+        let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
     }
+
 }
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
+    
     let mut window = Window::new(
         "Test - ESC to exit",
         WIDTH,
@@ -53,24 +58,17 @@ fn main() {
     .unwrap_or_else(|e| {
         panic!("{}", e);
     });
-    let render = Rendererxpto{};
-    //let events = Renderer::create_events_queue();
-    //let messages = hyber::Renderer::create_message_queue();
-    let events: Queue<hyber::Event> = Queue::new();
-    let messages: Queue<Message> = Queue::new();
-    Renderer::event_loop(events,messages);
+    let mut render = Rendererxpto{};
+    let events = render.create_events_queue();
+    let messages = render.create_message_queue();
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    render.event_loop(events, messages,&mut window);
     
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-    while window.is_open() && !window.is_key_down(Key::Escape) {
+    /*while window.is_open() && !window.is_key_down(Key::Escape) {
         if window.get_mouse_down(minifb::MouseButton::Left) {
-            //let event = Rendererxpto::map_events(EventoCliente::left_click);
-            //queue.enqueue(event);
-        }
-      
+            let event = Rendererxpto::map_events(EventoCliente::left_click);
+            queue.enqueue(event);
+        }*/
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
     }
-}
