17b734ea299198b399a39bad895243ae0eb799b5
MiguelLetra <migueljletra@gmail.com>
2021-01-03 17:33:21 +0000

    Add Tab Move event

--- src/bin/tab_view.rs
+++ src/bin/tab_view.rs
@@ -36,12 +36,7 @@ pub enum MessageXPTO {
       //  tab_ptr: Weak<RefCell<LabelWidget>>,
         event: Option<Event>,
     },
-    TabMoveLeft {
-        tab_grid_ptr: Weak<RefCell<GridViewWidget>>,
-        tab_ptr: Weak<RefCell<TabWidget>>,
-        event: Option<Event>,
-    },
-    TabMoveRight {
+    TabMove {
         tab_grid_ptr: Weak<RefCell<GridViewWidget>>,
         tab_ptr: Weak<RefCell<TabWidget>>,
         event: Option<Event>,
@@ -93,49 +88,44 @@ impl Message for MessageXPTO {
             } => {
               println!("Tab was pressed!");
             }
-            MessageXPTO::TabMoveLeft {
+            MessageXPTO::TabMove {
                 tab_grid_ptr,
                 tab_ptr,
                 event: _,
             } => {
-                println!("Tab was moved to the left!");
+                println!("Tab was Moved");
                 if let Some(tab) = tab_ptr.upgrade(){
                     if let Some(tab_grid) = tab_grid_ptr.upgrade(){
                         let movedTabID = tab.borrow_mut().id();
                         let mut index = 0;
-                        let mut index_to_be_moved = 999 ;
+                        let mut index_to_be_moved1 = 0;
+                        let mut index_to_be_moved2 = 0;
+                        let mut moved = false ;
                         for value in tab_grid.borrow_mut().get_children().iter_mut() {
                             if let Some(child) = value.upgrade() {
                                 let mut child= child.borrow_mut();
                                 //Check if it is a diferent tab than the one moved
                                 if child.id() != movedTabID{
-                                    //Check position of other tabs to the left
-                                    println!("X: {}",tab.borrow_mut().get_moved_cursor_pos().x);
-                                    if  tab.borrow_mut().get_moved_cursor_pos().x < (child.position().x +child.size().x/2. ) {
-                                        if index_to_be_moved == 999 {
-                                            index_to_be_moved = index;
+                                    //Check if mouse moved inside other tabs
+                                    if child.is_cursor_inside(tab.borrow_mut().get_moved_cursor_pos()){
+                                        if !moved{
+                                            index_to_be_moved1 = index;
+                                            moved = true;
                                         }
                                     }
-                                }//Once we get to the tab moved we should stop the search
+                                }
                                 else{
-                                    break;
+                                    index_to_be_moved2 = index;
                                 }
                             }
                             index = index + 1 ;
                         }
-                        if index_to_be_moved != 999{
-                            tab_grid.borrow_mut().get_children().swap(index_to_be_moved ,index );
+                        if moved {
+                            tab_grid.borrow_mut().get_children().swap(index_to_be_moved1 ,index_to_be_moved2);
                             tab_grid.borrow_mut().set_dirty(true);
                         }
                     }
                 }
-            }
-            MessageXPTO::TabMoveRight {
-                tab_grid_ptr,
-                tab_ptr,
-                event: _,
-            } => {
-                println!("Tab was moved to the right!");
             }
         }
     }
@@ -165,14 +155,7 @@ impl Message for MessageXPTO {
             } => {
                 *event = Some(new_event);
             }
-            MessageXPTO::TabMoveLeft {
-                tab_grid_ptr: _,
-                tab_ptr: _,
-                event,
-            } => {
-                *event = Some(new_event);
-            }
-            MessageXPTO::TabMoveRight {
+            MessageXPTO::TabMove {
                 tab_grid_ptr: _,
                 tab_ptr: _,
                 event,
@@ -223,26 +206,20 @@ fn main() {
             event: None,
         })),
         None,
-        None,
     )));
     
-    tab1.borrow_mut().setNewMessageMoveLeft(Some(Box::new(MessageXPTO::TabMoveLeft{
+    tab1.borrow_mut().setNewMessageMove(Some(Box::new(MessageXPTO::TabMove{
         tab_grid_ptr: Rc::downgrade(&tab_grid),
         tab_ptr: Rc::downgrade(&tab1),
         event: None,
     })));
 
-    tab1.borrow_mut().setNewMessageMoveRight(Some(Box::new(MessageXPTO::TabMoveRight{
-        tab_grid_ptr: Rc::downgrade(&tab_grid),
-        tab_ptr: Rc::downgrade(&tab1),
-        event: None,
-    })));
 
     let label_1 = Rc::new(RefCell::new(LabelWidget::new(
         String::from("Tab 1"),
         Vector2D::new(320., 200.),
         55,
-        Color::from_hex(0xff008026),
+        Color::from_hex(0x00008026),
         Color::from_hex(0xff004dff),
     )));
 
@@ -254,21 +231,21 @@ fn main() {
             event: None,
         })),
         None,
-        None,
     )));
     
-    tab2.borrow_mut().setNewMessageMoveLeft(Some(Box::new(MessageXPTO::TabMoveLeft{
+    tab2.borrow_mut().setNewMessageMove(Some(Box::new(MessageXPTO::TabMove{
         tab_grid_ptr: Rc::downgrade(&tab_grid),
         tab_ptr: Rc::downgrade(&tab2),
         event: None,
     })));
 
-    tab2.borrow_mut().setNewMessageMoveRight(Some(Box::new(MessageXPTO::TabMoveRight{
-        tab_grid_ptr: Rc::downgrade(&tab_grid),
-        tab_ptr: Rc::downgrade(&tab2),
-        event: None,
-    })));
-
+    let label_2 = Rc::new(RefCell::new(LabelWidget::new(
+        String::from("Tab 2"),
+        Vector2D::new(10., 200.),
+        55,
+        Color::from_hex(0x00008026),
+        Color::from_hex(0xff004dff),
+    )));
 
     let tab3 = Rc::new(RefCell::new(TabWidget::new(
         Vector2D::new(320., 200.),
@@ -278,26 +255,21 @@ fn main() {
             event: None,
         })),
         None,
-        None,
     )));
     
-    tab3.borrow_mut().setNewMessageMoveLeft(Some(Box::new(MessageXPTO::TabMoveLeft{
+    tab3.borrow_mut().setNewMessageMove(Some(Box::new(MessageXPTO::TabMove{
         tab_grid_ptr: Rc::downgrade(&tab_grid),
         tab_ptr: Rc::downgrade(&tab3),
         event: None,
     })));
 
-    tab3.borrow_mut().setNewMessageMoveRight(Some(Box::new(MessageXPTO::TabMoveRight{
-        tab_grid_ptr: Rc::downgrade(&tab_grid),
-        tab_ptr: Rc::downgrade(&tab3),
-        event: None,
-    })));
 
-    let label_2 = Rc::new(RefCell::new(LabelWidget::new(
-        String::from("Tab 2"),
+
+    let label_3 = Rc::new(RefCell::new(LabelWidget::new(
+        String::from("Tab 3"),
         Vector2D::new(10., 200.),
         55,
-        Color::from_hex(0xff008026),
+        Color::from_hex(0x00008026),
         Color::from_hex(0xff004dff),
     )));
 
@@ -326,8 +298,9 @@ fn main() {
 
     // definir relaçoes de parentesco
 
-    //tab1.borrow_mut().add_as_child(Rc::downgrade(&label_1) as Weak<RefCell<dyn Widget>>);
-   // tab2.borrow_mut().add_as_child(Rc::downgrade(&label_2) as Weak<RefCell<dyn Widget>>);
+    tab1.borrow_mut().add_as_child(Rc::downgrade(&label_1) as Weak<RefCell<dyn Widget>>);
+    tab2.borrow_mut().add_as_child(Rc::downgrade(&label_2) as Weak<RefCell<dyn Widget>>);
+    tab3.borrow_mut().add_as_child(Rc::downgrade(&label_3) as Weak<RefCell<dyn Widget>>);
     tab_grid.borrow_mut()
     .add_as_child(Rc::downgrade(&tab1) as Weak<RefCell<dyn Widget>>);
     tab_grid.borrow_mut()
