d27313f7059974780c78f4e47adad76315b80111
João Santos <jocatoca3@gmail.com>
2021-01-22 17:27:32 +0000

    Documentation update;

--- src/bin/tab_view.rs
+++ src/bin/tab_view.rs
@@ -1,3 +1,10 @@
+//! Contains the implementation of a `TabWidget` using the [`hyber`]
+//! (`crate`).
+//!
+//! The Tab View, on the [`hyber`](`crate`), is a widget implemented
+//! according  to the [`Widget`] trait but with his own properties.
+//! This properties need to be assigned by programmers.
+
 use hyber::display::Display;
 use hyber::event::Event;
 use hyber::renderer::{AbsoluteWidgetCollection, Message, RenderInstructionCollection, Renderer};
@@ -17,87 +24,44 @@ const WIDTH: f64 = 640.;
 /// The predefined display's height
 const HEIGHT: f64 = 360.;
 
-/// Messages that the [`TabWidget`] needs to handle
+/// Messages that the `TabWidget` needs to handle
 #[derive(Clone)]
 pub enum MessageXPTO {
-    Increment {
-        label_ptr: Weak<RefCell<LabelWidget>>,
-        num_ptr: Weak<RefCell<i64>>,
-        event: Option<Event>,
-    },
-    Decrement {
-        label_ptr: Weak<RefCell<LabelWidget>>,
-        num_ptr: Weak<RefCell<i64>>,
-        event: Option<Event>,
-    },
-    Resize {
-        grid_ptr: Weak<RefCell<GridViewWidget>>,
-        event: Option<Event>,
-    },
+    /// Message to handle a click on a Tab of the `TabWidget`
     TabPress {
         event: Option<Event>,
     },
+    /// Message to handle the dragging of a Tab of the `TabWidget`
+    /// by the user
     TabMove {
+        /// Reference to the memory location of the `GridViewWidget`
+        /// which contains the `TabWidget`
         tab_grid_ptr: Weak<RefCell<GridViewWidget>>,
+        /// Reference to the memory location of the `TabWidget`
         tab_ptr: Weak<RefCell<TabWidget>>,
+        /// Event that triggers this message
         event: Option<Event>,
     },
 }
 
-// t uconsegues :) we are rooting for you
 impl Message for MessageXPTO {
     fn update(&self) {
         match self {
-            MessageXPTO::Increment {
-                label_ptr,
-                num_ptr,
-                event: _,
-            } => {
-                if let Some(label) = label_ptr.upgrade() {
-                    if let Some(num) = num_ptr.upgrade() {
-                        *num.borrow_mut() += 1;
-                        label
-                            .borrow_mut()
-                            .set_text(String::from(format!("{}", *num.borrow())));
-                    }
-                }
-            }
-            MessageXPTO::Decrement {
-                label_ptr,
-                num_ptr,
-                event: _,
-            } => {
-                if let Some(label) = label_ptr.upgrade() {
-                    if let Some(num) = num_ptr.upgrade() {
-                        *num.borrow_mut() -= 1;
-                        label
-                            .borrow_mut()
-                            .set_text(String::from(format!("{}", *num.borrow())));
-                    }
-                }
-            }
-            MessageXPTO::Resize { grid_ptr, event } => {
-                /*if let Some(grid) = grid_ptr.upgrade() {
-                    if let Some(Event::Mouse(CursorMoved { x, y })) = event {
-                        grid.borrow_mut().set_original_size(Vector2D::new(*x as f64, *y as f64))
-                    }
-                }*/
-            }
             MessageXPTO::TabPress {
-                //    tab_ptr,
                 event: _,
             } => {
-                println!("Tab was pressed!");
+                // Not used.
             }
             MessageXPTO::TabMove {
                 tab_grid_ptr,
                 tab_ptr,
                 event: _,
             } => {
-                println!("Tab was Moved");
+                // Gets the memory reference of the `LabelWidget`
                 if let Some(tab) = tab_ptr.upgrade() {
+                    // Gets the memory reference of the `TabWidget`
                     if let Some(tab_grid) = tab_grid_ptr.upgrade() {
-                        let movedTabID = tab.borrow_mut().id();
+                        let moved_tab_id = tab.borrow_mut().id();
                         let mut index = 0;
                         let mut index_to_be_moved1 = 0;
                         let mut index_to_be_moved2 = 0;
@@ -106,7 +70,7 @@ impl Message for MessageXPTO {
                             if let Some(child) = value.upgrade() {
                                 let mut child = child.borrow_mut();
                                 //Check if it is a diferent tab than the one moved
-                                if child.id() != movedTabID {
+                                if child.id() != moved_tab_id {
                                     //Check if mouse moved inside other tabs
                                     if child
                                         .is_cursor_inside(tab.borrow_mut().get_moved_cursor_pos())
@@ -137,25 +101,7 @@ impl Message for MessageXPTO {
 
     fn set_event(&mut self, new_event: Event) {
         match self {
-            MessageXPTO::Increment {
-                label_ptr: _,
-                num_ptr: _,
-                event,
-            } => {
-                *event = Some(new_event);
-            }
-            MessageXPTO::Decrement {
-                label_ptr: _,
-                num_ptr: _,
-                event,
-            } => {
-                *event = Some(new_event);
-            }
-            MessageXPTO::Resize { grid_ptr: _, event } => {
-                *event = Some(new_event);
-            }
             MessageXPTO::TabPress {
-                //     tab_ptr: _,
                 event,
             } => {
                 *event = Some(new_event);
@@ -172,6 +118,7 @@ impl Message for MessageXPTO {
 }
 
 fn main() {
+    // Sets up the display using the [`minifb`](`crate`)
     let mut display = hyber_raqote::DisplayMinifb::new(
         "Test - ESC to exit",
         WIDTH as usize,
@@ -182,36 +129,33 @@ fn main() {
         },
     );
 
+    // Sets up the identifier to this machine
     let mut id_machine = IDMachine::new();
 
+    // Sets up the collection to hold all the render instructions required
     let collection = Rc::new(RefCell::new(RenderInstructionCollection::new()));
 
+    // Sets up the absolute widgets collection
     let absolute_collection = Rc::new(RefCell::new(AbsoluteWidgetCollection::new()));
 
-    let grid = Rc::new(RefCell::new(GridViewWidget::new(
-        Vector2D::new(WIDTH, HEIGHT),
-        Axis::Vertical,
-        3,
-    )));
-
-    let counter = Rc::new(RefCell::new(0));
-
+    // Initializes the `GridViewWidget` to hold multiple `TabWidget`
     let tab_grid = Rc::new(RefCell::new(GridViewWidget::new(
         Vector2D::new(500., 200.),
         Axis::Vertical,
         2,
     )));
 
+    // Initializes the `TabWidget` #01
     let tab1 = Rc::new(RefCell::new(TabWidget::new(
         Vector2D::new(320., 200.),
         Color::from_hex(0xff509996),
         Some(Box::new(MessageXPTO::TabPress {
-            //tab_grid_ptr: Rc::downgrade(&tab1),
             event: None,
         })),
         None,
     )));
 
+    // Assigns the message `TabMove` to the `TabWidget` #01
     tab1.borrow_mut()
         .set_new_message_move(Some(Box::new(MessageXPTO::TabMove {
             tab_grid_ptr: Rc::downgrade(&tab_grid),
@@ -219,6 +163,7 @@ fn main() {
             event: None,
         })));
 
+    // Initializes the `LabelWidget` #01 to display the text within the `TabWidget` #01
     let label_1 = Rc::new(RefCell::new(LabelWidget::new(
         String::from("Tab 1"),
         Vector2D::new(320., 200.),
@@ -227,22 +172,25 @@ fn main() {
         Color::from_hex(0xff004dff),
     )));
 
+    // Initializes the `TabWidget` #02
     let tab2 = Rc::new(RefCell::new(TabWidget::new(
         Vector2D::new(320., 200.),
         Color::from_hex(0xffd15c20),
         Some(Box::new(MessageXPTO::TabPress {
-            //tab_grid_ptr: Rc::downgrade(&tab1),
             event: None,
         })),
         None,
     )));
 
+    // Assigns the message `TabMove` to the `TabWidget` #02
     tab2.borrow_mut()
         .set_new_message_move(Some(Box::new(MessageXPTO::TabMove {
             tab_grid_ptr: Rc::downgrade(&tab_grid),
             tab_ptr: Rc::downgrade(&tab2),
             event: None,
         })));
+
+    // Initializes the `LabelWidget` #02 to display the text within the `TabWidget` #02
     let label_2 = Rc::new(RefCell::new(LabelWidget::new(
         String::from("Tab 2"),
         Vector2D::new(10., 200.),
@@ -251,16 +199,17 @@ fn main() {
         Color::from_hex(0xff004dff),
     )));
 
+    // Initializes the `TabWidget` #03
     let tab3 = Rc::new(RefCell::new(TabWidget::new(
         Vector2D::new(320., 200.),
         Color::from_hex(0xffd15390),
         Some(Box::new(MessageXPTO::TabPress {
-            //tab_grid_ptr: Rc::downgrade(&tab1),
             event: None,
         })),
         None,
     )));
 
+    // Assigns the message `TabMove` to the `TabWidget` #03
     tab3.borrow_mut()
         .set_new_message_move(Some(Box::new(MessageXPTO::TabMove {
             tab_grid_ptr: Rc::downgrade(&tab_grid),
@@ -268,6 +217,7 @@ fn main() {
             event: None,
         })));
 
+    // Initializes the `LabelWidget` #03 to display the text within the `TabWidget` #03
     let label_3 = Rc::new(RefCell::new(LabelWidget::new(
         String::from("Tab 3"),
         Vector2D::new(10., 200.),
@@ -276,46 +226,51 @@ fn main() {
         Color::from_hex(0xff004dff),
     )));
 
+    // Initializes the `RootWidget` to handle the widgets tree
     let root = Rc::new(RefCell::new(RootWidget::new(
         display.get_size(),
         Color::new(0xff, 0xff, 0xff, 0xff),
         Layout::Box(Axis::Horizontal),
-        Box::new(MessageXPTO::Increment {
-            label_ptr: Rc::downgrade(&label_1),
-            num_ptr: Rc::downgrade(&counter),
-            event: None,
-        }),
-        Box::new(MessageXPTO::Decrement {
-            label_ptr: Rc::downgrade(&label_1),
-            num_ptr: Rc::downgrade(&counter),
-            event: None,
-        }),
     )));
 
-    // definir relaçoes de parentesco
-
+    // The next instructions build the widgets relative tree
+    
+    // Adds the `LabelWidget` #01 as child of the `TabWidget` #01
     tab1.borrow_mut()
         .add_as_child(Rc::downgrade(&label_1) as Weak<RefCell<dyn Widget>>);
+    // Adds the `LabelWidget` #02 as child of the `TabWidget` #02
     tab2.borrow_mut()
         .add_as_child(Rc::downgrade(&label_2) as Weak<RefCell<dyn Widget>>);
+    // Adds the `LabelWidget` #03 as child of the `TabWidget` #03
     tab3.borrow_mut()
         .add_as_child(Rc::downgrade(&label_3) as Weak<RefCell<dyn Widget>>);
+
+    // Adds the `TabWidget` #01 as child of the `GridViewWidget`
     tab_grid
         .borrow_mut()
         .add_as_child(Rc::downgrade(&tab1) as Weak<RefCell<dyn Widget>>);
+    // Adds the `TabWidget` #02 as child of the `GridViewWidget`
     tab_grid
         .borrow_mut()
         .add_as_child(Rc::downgrade(&tab2) as Weak<RefCell<dyn Widget>>);
+    // Adds the `TabWidget` #03 as child of the `GridViewWidget`
     tab_grid
         .borrow_mut()
         .add_as_child(Rc::downgrade(&tab3) as Weak<RefCell<dyn Widget>>);
+
+    // Adds the `GridViewWidget` as child of the `RootWidget`
     root.borrow_mut()
         .add_as_child(Rc::downgrade(&tab_grid) as Weak<RefCell<dyn Widget>>);
 
+    // Initializes the renderer built with [`raqote`](`crate`)  
     let mut renderer = hyber_raqote::Raqote::new(WIDTH as i32, HEIGHT as i32);
     let events = renderer.create_events_queue();
     let messages = renderer.create_message_queue();
 
+    // Initializes the main renderer loop
+    // This loop makes it possible to handle all the events
+    // and user interactions, as well as manages the widgets
+    // that are being displayed
     renderer.event_loop(
         events,
         messages,
@@ -326,11 +281,4 @@ fn main() {
         Rc::downgrade(&collection),
         Rc::downgrade(&absolute_collection),
     );
-    // Limit to max ~60 fps update rate
-    /*while window.is_open() && !window.is_key_down(Key::Escape) {
-    if window.get_mouse_down(minifb::MouseButton::Left) {
-        let event = Rendererxpto::map_events(EventoCliente::left_click);
-        queue.enqueue(event);
-    }*/
-    // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
 }
